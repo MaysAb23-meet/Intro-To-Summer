2026c24a5e988b48e6b8cc4d8d9eb9ce4c683f03
MaysAb23-meet <109899447+MaysAb23-meet@users.noreply.github.com>
2022-07-24 14:43:37 +0300

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,3 @@
+print("hello Y2")
+print("hello")
+print("heyy I'm Mays Abdelgafer,I'm super thrilled about starting y2 summer and I'm very happy to meet you and can't wait to have agreat summer together and get to know you<3 ")
